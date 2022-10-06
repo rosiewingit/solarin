4e12d04ef7ec4265dd50b92635fe092bf5e9b25b
Rosie <Rosie@Rosieui-MacBook-Pro.local>
2022-10-07 07:07:32 +0900

    Update texts

--- theme3/src/main.js
+++ theme3/src/main.js
@@ -1,15 +1,42 @@
 let root = null;
 
 const host = "https://rosiewingit.github.io/solarin/theme3/resources/";
-const mainPages = [`main-page-1`, `main-page-2`, `main-page-3`, `main-page-4`];
+const mainPageItems = [
+  {
+    id: "main-page-1",
+    title: "Make convenient with new ideas.",
+    subtitle:
+      "새로운 생각으로 일상을 더 편리하게, 아이디어를 현실로 만드는 기업 솔라인.",
+  },
+  {
+    id: "main-page-2",
+    title: "Technology for Human.",
+    subtitle:
+      "새로운 생각으로 일상을 더 편리하게, 아이디어를 현실로 만드는 기업 솔라인.",
+  },
+  {
+    id: "main-page-3",
+    title: "Idea Design & Manufacturing",
+    subtitle:
+      "새로운 생각으로 일상을 더 편리하게, 아이디어를 현실로 만드는 기업 솔라인.",
+  },
+  {
+    id: "main-page-4",
+    title: "New Challenge for Future.",
+    subtitle:
+      "새로운 생각으로 일상을 더 편리하게, 아이디어를 현실로 만드는 기업 솔라인.",
+  },
+];
 
 window.onload = () => {
   console.log("ONLOAD");
 
   root = $("#root");
-  mainPages.forEach((page) => {
-    const content = new mainContent(page);
+  mainPageItems.forEach((page) => {
+    const content = new mainContent(page.id);
     content.init();
+    content.setBackground();
+    content.setTextContents(page.title, page.subtitle);
   });
 };
 
@@ -21,16 +48,26 @@ class mainContent {
 
   init() {
     root.append(this.createSection());
-    this.setBackground();
   }
 
   createSection() {
     return `
-    <section id="${this.id}" class="main-body-content"></section>
+    <section id="${this.id}" class="main-body-content">
+      <article id="${this.id}-article" class="main-body-item">
+        <h1 class="main-body-item-title">Make convenient with new ideas.</h1>
+        <p class="main-body-item-subtitle">새로운 생각으로 일상을 더 편리하게, 아이디어를 현실로 만드는 기업 솔라인</p>
+      </article>
+    </section>
     `;
   }
 
   setBackground() {
     $(`#${this.id}`).css("background-image", `url(${this.imageUrl})`);
+  }
+
+  setTextContents(title, subtitle) {
+    const article = $(`#${this.id}-article`);
+    article.find("h1").text(title);
+    article.find("p").text(subtitle);
   }
 }
