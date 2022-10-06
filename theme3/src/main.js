36a1463df50ce2072e9217da3aa91a4fb8c83530
Rosie <Rosie@Rosieui-MacBook-Pro.local>
2022-10-06 20:18:12 +0900

    Add mainPage layouts

--- theme3/src/main.js
+++ theme3/src/main.js
@@ -1,3 +1,35 @@
+let root = null;
+
+const host = "https://rosiewingit.github.io/solarin/theme3/resources/";
+const mainPages = [`main-page-1`, `main-page-2`, `main-page-3`, `main-page-4`];
+
 window.onload = () => {
   console.log("ONLOAD");
+
+  root = $("#root");
+  mainPages.forEach((page) => {
+    const content = new mainContent(page);
+    content.init();
+  });
 };
+
+class mainContent {
+  constructor(id) {
+    this.id;
+    this.imageUrl = `${host}${id}.png`;
+  }
+
+  init() {
+    root.append(this.createSection());
+  }
+
+  createSection() {
+    return `
+    <section id="${this.id}" class="main-body-content"></section>
+    `;
+  }
+
+  setBackground() {
+    $(`#${this.id}`).css("background-image", `url(${this.imageUrl})`);
+  }
+}
