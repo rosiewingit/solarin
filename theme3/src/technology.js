4310aa51d40ceea9501005443c84c033eaa8cd68
Rosie <Rosie@Rosieui-MacBook-Pro.local>
2022-10-09 23:49:19 +0900

    Remove redundent

--- theme3/src/technology.js
+++ theme3/src/technology.js
@@ -1,28 +1,51 @@
 const clickRndCenter = () => {
   $("#root").addClass("hidden");
-  $("#technologyPage").removeClass("hidden");
+  $("#patentPage").addClass("hidden");
+  $("#rndcenterPage").removeClass("hidden");
   setHeaderOpacity(1);
   setNavDropdownOpacity(0.7);
 
-  const content = new PPTContent("rnd-page-1");
+  const rndcenterPageId = "#rndcenterPage";
+  const content = new PPTContent(rndcenterPageId, "rnd-page-1");
   content.init();
+  content.setPath("Home > Technology > <b>R&D Center</b>");
+};
+
+const clickPatent = () => {
+  $("#root").addClass("hidden");
+  $("#rndcenterPage").addClass("hidden");
+  $("#patentPage").removeClass("hidden");
+
+  const rndcenterPageId = "#patentPage";
+  const content = new PPTContent(rndcenterPageId, "patent-page-1");
+  content.init();
+  content.setPath("Home > Technology > <b>Patent</b>");
 };
 
 class PPTContent {
-  constructor(id) {
+  constructor(page, id) {
+    this.page = page;
     this.id = id;
   }
 
   init() {
-    $("#technologyPage").append(this.createSection());
+    if ($(`#${this.id}`).length > 0) {
+      return;
+    }
+    $(this.page).append(this.createSection());
   }
 
   createSection() {
     return `
     <section id="${this.id}" class="technology-body-content">
-      <div class="ppt-path">Home > Technology > R&D Center</div>
+      <div class="ppt-path">Home > Technology > <b>R&D Center</b></div>
       <hr class="ppt-hr" />
+      <article id="${this.id}-article"></article>
     </section>
     `;
+  }
+
+  setPath(path) {
+    $(`#${this.id}`).find(".ppt-path").html(path);
   }
 }
