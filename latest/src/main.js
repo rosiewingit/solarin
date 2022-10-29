d9f92d8f38a40212985173bf61d66e881b878a4a
Rosie <rosiewingit@gmail.com>
2022-10-29 22:48:26 +0900

    Update rnd center

--- latest/src/main.js
+++ latest/src/main.js
@@ -29,7 +29,7 @@ const mainPageItems = [
 window.onload = () => {
   console.log("ONLOAD");
 
-  // clickCoreTechnology();
+  // clickRndCenter();
   showPage(rootPageId);
 
   mainPageItems.forEach((page) => {
