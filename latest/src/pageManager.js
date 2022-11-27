4269f2c852230bc287c8846d0d88f1b327b5f47d
Rosie <rosiewingit@gmail.com>
2022-11-27 18:03:39 +0900

    Update devlopment image

--- latest/src/pageManager.js
+++ latest/src/pageManager.js
@@ -279,21 +279,21 @@ class PPTContent {
           <div class="development-card">
             <h1 class="development-title">
               <div class="title-bar-top-line"></div>
-              제품명 1
+              어망절단기
             </h1>
             <div id="developmentCard1" class="development-image"></div>
           </div>
           <div class="development-card">
             <h1 class="development-title">
               <div class="title-bar-top-line"></div>
-              제품명 2
+              펌프
             </h1>
             <div id="developmentCard2" class="development-image"></div>
           </div>
           <div class="development-card">
             <h1 class="development-title">
               <div class="title-bar-top-line"></div>
-              제품명 3
+              자동사다리
             </h1>
             <div id="developmentCard3" class="development-image"></div>
           </div>
